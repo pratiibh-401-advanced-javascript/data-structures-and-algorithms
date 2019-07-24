5f1f5158eaaf585119179c9c040cfcd97ab14812
Pratiibh <pratiibhbassi@gmail.com>
2019-07-23 23:38:13 -0700

    readme updated for queue_with_stacks

---
+++ Data-Structures/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Stack = require('../stacksandQueues/stacks-and-queues.js').Stack;
+
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class PseudoQueue{
+  constructor(){
+    let stackOne = new PseudoQueue();
+    let stackTwo = new PseudoQueue();
+  }
+
+}
+
+
+
